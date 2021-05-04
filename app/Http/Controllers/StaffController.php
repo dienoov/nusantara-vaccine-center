fb10686ea1e7f01cb49bbdeadeffc147ba0a1812
dienoov <alvinodien@yahoo.co.id>
2021-05-04 13:24:57 +0700

    Staff CRUD

---
+++ app/Http/Controllers/StaffController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Staff;
+use Illuminate\Http\Request;
+
+class StaffController extends Controller
+{
+    public function all()
+    {
+        return response([
+            'staff' => Staff::all(),
+            'message' => 'Success',
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'name' => 'required|string',
+            'password' => 'required|min:8|confirmed',
+            'vac_center_id' => 'required|exists:vac_centers,id',
+        ]);
+
+        $input = $request->only(['username', 'name', 'vac_center_id']);
+        $input['password'] = bcrypt($request->password);
+
+        $staff = Staff::create($input);
+
+        return response([
+            'staff' => $staff,
+            'message' => 'Success',
+        ]);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'vac_center_id' => 'required|exists:vac_centers,id',
+        ]);
+
+        $input = $request->only(['name', 'vac_center_id']);
+
+        $staff = Staff::find($id);
+        $staff->update($input);
+
+        return response([
+            'staff' => $staff,
+            'message' => 'Success',
+        ]);
+    }
+
+    public function delete(int $id)
+    {
+        Staff::destroy($id);
+
+        return response([
+            'message' => 'Success',
+        ]);
+    }
+}
